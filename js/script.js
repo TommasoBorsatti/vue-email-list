81c0e69137be01161c5f09f19ff6aa52dad187cc
Tommaso Borsatti <tommasoborsatti89@gmail.com>
2021-04-07 15:11:15 +0200

    Esercizio finito.
    Fatto alcuni test con chiamate AJAX tramite AXIOS e VUE; fatto test con arrow function & con self = this.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,31 @@
 
-const app = new Vue(
+const App = new Vue({
 
-{
   el: "#root",
 
   data:{
+    eMail: [],
+  },
+
+  mounted: function(){
+
+    // METODO CON SELF = THIS
+
+    // let self = this;
+    //
+    // for (var i = 0; i < 10; i++) {
+    //   axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(function (email) {
+    //     self.eMail.push(email.data.response);
+    //   });
+    // };
+
+    // METODO CON ARROW FUNCTION
+
+    for (var i = 0; i < 10; i++) {
+      axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then ((email) => {
+        this.eMail.push(email.data.response);
+      });
+    };
 
   },
 
@@ -12,10 +33,4 @@ const app = new Vue(
 
   },
 
-  mounted:{
-
-  },
-  
-}
-
-);
+});
